126c9aee7b0cd3f9ffc93fa770a154aa3649a92e
Daniel Newns <dan@jump24.co.uk>
2023-05-19 10:52:44 +0100

    Initial Commit

---
+++ WhitbyLandmarks/WhitbyLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhitbyLandmarksApp.swift
+//  WhitbyLandmarks
+//
+//  Created by dannewns on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhitbyLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
